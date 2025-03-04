fcc1b0ed16511ff77dde3a935527e29adb376fff
Llander Arroz <arroz.llandersce1965@gmail.com>
2025-03-04 14:58:23 +0800

    test

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,11 @@
+import { NavLink } from "react-router-dom";
 export default function Home(){
     return(
         <div className="container">
             <h1>
                 Home
             </h1>
-            <a href="./selection">Start</a>
+            <NavLink to="./selection">Start</NavLink>
             <br />
             <a href="/leaderBoards">Leader Boards</a>
         </div>
